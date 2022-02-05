9c00bd583170560a6d386fc1e39e73f114fc3639
Aleksandr-Ljabin <frontdevell@gmail.com>
2022-02-05 14:31:45 +0300

    Решение

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+class Validator {
+  constructor(name) {
+    this.name = name;
+  }
+
+  validateUsername() {
+    const regOne = /^[^-_0-9][a-z0-9-_]+[^-_0-9]$/igm;
+    const regTwo = /\d{4,}/gm;
+    if (regOne.test(this.name) === true && regTwo.test(this.name) === false) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
